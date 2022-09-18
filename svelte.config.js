e18fd3cc4d2f05622a143c01cc09f94715f0f4ab
DiamonC <88857034+DiamonC@users.noreply.github.com>
2022-09-17 20:41:23 -0500

    installed tailwind & daisyui

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,13 @@
+import preprocess from "svelte-preprocess";
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: [
+		preprocess({
+		  postcss: true,
+		}),
+	  ],
 	kit: {
 		adapter: adapter()
 	}
