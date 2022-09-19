e8fecb78887942fd8a51847bc5ba5a586a6d5005
Jade Nash <jade.nashh18@gmail.com>
2022-09-19 14:28:13 -0500

    new icons, store theme in local storage, prettier

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,17 @@
-import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from "svelte-preprocess"
+import adapter from "@sveltejs/adapter-auto"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		preprocess({
-		  postcss: true,
-		}),
-	  ],
-	kit: {
-		adapter: adapter()
-	}
-};
+    preprocess: [
+        preprocess({
+            postcss: true,
+            scss: true,
+        }),
+    ],
+    kit: {
+        adapter: adapter(),
+    },
+}
 
-export default config;
+export default config
