e1485aa0221f1bc5370a8cdd6c88f846e87249c0
DiamonC <88857034+DiamonC@users.noreply.github.com>
2022-09-19 14:15:12 -0500

    installed prettier npm package

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -62,9 +62,9 @@ onMount(() => {
 	</div>
 	<label tabindex="0" class="btn btn-ghost btn-circle">
         <div class="indicator">
-          <a href="/pay"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg></a>
-        </div>
-      </label>
+          	<a href="/pay"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg></a>
+        			</div>
+      	</label>
 	  <div>
 		<button class="text-base-content btn btn-circle text-3xl btn-ghost">
 			<a href="/newserver">+</a>
@@ -75,7 +75,6 @@ onMount(() => {
 		<div class=" ring ring-primary bg-neutral-focus text-neutral-content rounded-full w-10">
 		  <a href="/login"><span class="text-2xl">EX</span></a>
 		</div>
-
 	  </div> 
 	</div>
   </div>
