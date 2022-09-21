b3725c36d4d26bdc65e0463177c52ef2d78e4986
DiamonC <88857034+DiamonC@users.noreply.github.com>
2022-09-21 12:37:37 -0500

    login changes

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -11,22 +11,32 @@
   });
 </script>
 
-<h2 class="mt-5 mb-2 text-4xl font-extrabold md:text-6xl">
-  Login to Arth Hosting
-</h2>
-<div class="mt-5">
-  <button class="btn  btn-primary"
-    ><img
-      alt="ms_logo"
-      style="width:2.5ch"
-      src="https://microsoft.com/favicon.ico"
-    />Sign in with Microsoft</button
-  >
-  <button class="btn  btn-primary"
-    ><img
-      alt="ms_logo"
-      style="width:2.5ch"
-      src="https://metamask.io/icons/icon-96x96.png?v=48400a28770e10dd52a8c0e539aeb282"
-    />Sign in with MetaMask</button
-  >
+<div class="hero min-h-screen bg-base-200">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Welcome Back!</h1>
+      <p class="py-6">
+        Log into your account or <a
+          class="link link-primary"
+          href="https://duck.com">sign up.</a
+        >
+      </p>
+      <div class="mt-5">
+        <button class="btn  btn-primary"
+          ><img
+            alt="ms_logo"
+            style="width:2.5ch"
+            src="https://microsoft.com/favicon.ico"
+          />Login with Microsoft</button
+        >
+        <button class="btn  btn-primary"
+          ><img
+            alt="ms_logo"
+            style="width:2.5ch"
+            src="https://metamask.io/icons/icon-96x96.png?v=48400a28770e10dd52a8c0e539aeb282"
+          />Login with MetaMask</button
+        >
+      </div>
+    </div>
+  </div>
 </div>
