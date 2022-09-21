f390918d2a9cb15213803674227586e5ad93354c
Jade Nash <jade.nashh18@gmail.com>
2022-09-21 17:58:21 -0500

    add billing alert for non-commercial users

--- src/routes/(login)/billing/+page.svelte
+++ src/routes/(login)/billing/+page.svelte
@@ -1,8 +1,37 @@
 <script lang="ts">
-    //
+    import { fade } from "svelte/transition"
+
+    let alertOpen: boolean = true
 </script>
 
 <div class="flex place-content-center">
+    {#if alertOpen}
+        <div class="fixed top-20 w-[95%]" out:fade>
+            <div class="alert alert-info flex justify-between shadow-lg">
+                <div>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        class="stroke-current flex-shrink-0 w-6 h-6"
+                        ><path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                        /></svg
+                    >
+                    <span
+                        >If you're self-hosting, you may not need this page!</span
+                    >
+                </div>
+                <button
+                    class="btn btn-ghost btn-sm"
+                    on:click={() => (alertOpen = false)}>Dismiss</button
+                >
+            </div>
+        </div>
+    {/if}
     <div class="flex flex-col grow items-center max-w-[55rem] mb-10 space-y-6">
         <h1 class="divider px-10 text-3xl font-semibold">Billing</h1>
         <div
