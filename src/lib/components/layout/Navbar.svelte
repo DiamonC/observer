f081447fcee788320b0f3b617cd54403589db75b
Jade Nash <jade.nashh18@gmail.com>
2022-09-21 12:30:26 -0500

    remove unnecessary tailwind class

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -136,9 +136,7 @@
                     tabindex="0"
                     class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-200 rounded-box w-52"
                 >
-                    <li>
-                        <a class="justify-between"> Profile </a>
-                    </li>
+                    <li><a>Profile</a></li>
                     <li><a>Account Settings</a></li>
                     <li><a>Logout</a></li>
                 </ul>
