0230537de3bec3b6e99a7bd6ad7cb1c40d17e0e3
Jade Nash <jade.nashh18@gmail.com>
2022-09-21 12:09:05 -0500

    new server page rework, layout & color changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,16 +4,19 @@
 
     import { onMount } from "svelte"
     import { themeChange } from "theme-change"
+    import Footer from "$lib/components/layout/Footer.svelte"
 
     onMount(() => {
         themeChange(false)
         // 👆 false parameter is required for svelte
     })
+    // will break if not used here
 </script>
 
-<div>
+<div class="flex flex-col justify-between h-[100vh]">
     <Navbar />
     <div class="m-8">
         <slot />
     </div>
+    <Footer />
 </div>
