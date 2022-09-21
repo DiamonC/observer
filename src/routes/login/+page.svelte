0b06ab15a5ef0173f56688fc701a2bc64be513dd
DiamonC <88857034+DiamonC@users.noreply.github.com>
2022-09-21 10:02:42 -0500

    fixed darkmode on /pay + /login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,19 +1,32 @@
+<script lang="ts">
+  import darkMode from "$lib/stores/darkMode";
+  import { onMount } from "svelte";
+  import { missing_component } from "svelte/internal";
+  import { themeChange } from "theme-change";
+
+  // NOTE: the element that is using one of the theme attributes must be in the DOM on mount
+  onMount(() => {
+    themeChange(false);
+    // 👆 false parameter is required for svelte
+  });
+</script>
+
 <h2 class="mt-5 mb-2 text-4xl font-extrabold md:text-6xl">
-    Login to Arth Hosting
+  Login to Arth Hosting
 </h2>
 <div class="mt-5">
-    <button class="btn  btn-primary"
-        ><img
-            alt="ms_logo"
-            style="width:2.5ch"
-            src="https://microsoft.com/favicon.ico"
-        />Sign in with Microsoft</button
-    >
-    <button class="btn  btn-primary"
-        ><img
-            alt="ms_logo"
-            style="width:2.5ch"
-            src="https://metamask.io/icons/icon-96x96.png?v=48400a28770e10dd52a8c0e539aeb282"
-        />Sign in with MetaMask</button
-    >
+  <button class="btn  btn-primary"
+    ><img
+      alt="ms_logo"
+      style="width:2.5ch"
+      src="https://microsoft.com/favicon.ico"
+    />Sign in with Microsoft</button
+  >
+  <button class="btn  btn-primary"
+    ><img
+      alt="ms_logo"
+      style="width:2.5ch"
+      src="https://metamask.io/icons/icon-96x96.png?v=48400a28770e10dd52a8c0e539aeb282"
+    />Sign in with MetaMask</button
+  >
 </div>
--- src/routes/newserver/+page.svelte
+++ src/routes/newserver/+page.svelte
@@ -1,68 +1,78 @@
 <script lang="ts">
-    let serverVis = "none"
+  let serverVis = "none";
+  import darkMode from "$lib/stores/darkMode";
+  import { onMount } from "svelte";
+  import { missing_component } from "svelte/internal";
+  import { themeChange } from "theme-change";
+
+  // NOTE: the element that is using one of the theme attributes must be in the DOM on mount
+  onMount(() => {
+    themeChange(false);
+    // 👆 false parameter is required for svelte
+  });
 </script>
 
 <div id="SoftwareMenu" class="dropdown dropdown-right dropdown-end">
-    <form>
-        <label for="softwaredd">Choose a software:</label>
-        <select
-            id="softwaredd"
-            name="softwaredd"
-            class="dropdown-content menu p-2 shadow bg-base-400 rounded-box w-52"
-        >
-            <option value="snapshot">Servers</option>
-            <option value="spigot">Proxies</option>
-            <option value="purpur">Modded</option>
-            <option value="paper">Vanilla</option>
-            <option value="tunity">Bedrock</option>
-        </select>
-    </form>
+  <form>
+    <label for="softwaredd">Choose a software:</label>
+    <select
+      id="softwaredd"
+      name="softwaredd"
+      class="dropdown-content menu p-2 shadow bg-base-400 rounded-box w-52"
+    >
+      <option value="snapshot">Servers</option>
+      <option value="spigot">Proxies</option>
+      <option value="purpur">Modded</option>
+      <option value="paper">Vanilla</option>
+      <option value="tunity">Bedrock</option>
+    </select>
+  </form>
 </div>
 
 <div id="serverForm">
-    <form>
-        <div class="dropdown dropdown-down dropdown-right">
-            <label tabindex="0" class="btn m-1">Choose ></label>
-            <ul
-                tabindex="0"
-                class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-            >
-                <div class="divider">Featured</div>
-                <li><a>Paper</a></li>
-                <li><a>Velocity</a></li>
-                <li><a>Waterfall</a></li>
-                <li><a>Forge</a></li>
-                <div class="divider">Other</div>
-                <li><a>Fabric</a></li>
-                <li><a>Mohist</a></li>
-                <li><a>Snapshot</a></li>
-                <li><a>Spigot</a></li>
-            </ul>
-        </div>
+  <form>
+    <div class="dropdown dropdown-down dropdown-right">
+      <label tabindex="0" class="btn m-1">Choose ></label>
+      <ul
+        tabindex="0"
+        class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <div class="divider">Featured</div>
+        <li><a>Paper</a></li>
+        <li><a>Velocity</a></li>
+        <li><a>Waterfall</a></li>
+        <li><a>Forge</a></li>
+        <div class="divider">Other</div>
+        <li><a>Fabric</a></li>
+        <li><a>Mohist</a></li>
+        <li><a>Snapshot</a></li>
+        <li><a>Spigot</a></li>
+      </ul>
+    </div>
 
-        <label class="label" for="1">Server Name:</label>
-        <input
-            id="1"
-            class="input-bordered input-primary input w-full max-w-xs bg-base-300"
-            type="text"
-            placeholder="ex: My Minecraft Server"
-        />
+    <label class="label" for="1">Server Name:</label>
+    <input
+      id="1"
+      class="input-bordered input-primary input w-full max-w-xs bg-base-300"
+      type="text"
+      placeholder="ex: My Minecraft Server"
+    />
 
-        <label class="label" for="2">Technical Server Name:</label>
-        <input
-            id="2"
-            class="input-bordered input-primary input w-full max-w-xs bg-base-300"
-            type="text"
-            placeholder="ex: my_minecraft_server"
-        />
+    <label class="label" for="2">Technical Server Name:</label>
+    <input
+      id="2"
+      class="input-bordered input-primary input w-full max-w-xs bg-base-300"
+      type="text"
+      placeholder="ex: my_minecraft_server"
+    />
 
-        <label class="label" for="3">Minecraft version:</label>
-        <input
-            id="3"
-            class="input-bordered input-primary input w-full max-w-xs bg-base-300"
-            type="text"
-            placeholder="ex: latest, 1.16.5"
-        />
-        <div><input type="submit" value="Create" class="btn" /></div>
-    </form>
+    <label class="label" for="3">Minecraft version:</label>
+    <input
+      id="3"
+      class="input-bordered input-primary input w-full max-w-xs bg-base-300"
+      type="text"
+      placeholder="ex: latest, 1.16.5"
+    />
+    <div><input type="submit" value="Create" class="btn" /></div>
+  </form>
 </div>
