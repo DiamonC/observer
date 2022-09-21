4dee79852e98cfc05ab3ee8b869c55a9ea261fbe
Jade Nash <jade.nashh18@gmail.com>
2022-09-21 17:46:48 -0500

    made helper button div instead

--- src/lib/components/ui/Helper.svelte
+++ src/lib/components/ui/Helper.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="tooltip tooltip-bottom" data-tip={tooltipText}>
-    <button class="btn btn-circle btn-ghost btn-md">
+    <div class="btn btn-circle btn-ghost btn-md">
         <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -19,5 +19,5 @@
                 d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"
             /><line x1="12" y1="17" x2="12.01" y2="17" /></svg
         >
-    </button>
+    </div>
 </div>
