48dcd58fd514ebee5d0986b8d5ab81ff51a1436a
DiamonC <88857034+DiamonC@users.noreply.github.com>
2022-09-21 13:01:40 -0500

    trusted domains

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,21 +1,36 @@
 <div id="settingsForm">
-    <form>
-        <label class="label" for="3">Webpanel URL:</label>
-        <input
-            id="3"
-            class="input-bordered input-primary input w-full max-w-xs bg-base-300"
-            type="text"
-            placeholder="ex: https://localhost:5173"
-        />
+  <form>
+    <label class="label" for="3">Webpanel URL:</label>
+    <input
+      id="3"
+      class="input-bordered input-primary input w-full max-w-xs bg-base-300"
+      type="text"
+      placeholder="ex: https://localhost:5173"
+    />
 
-        <label class="label" for="3">Webpanel Title:</label>
-        <input
-            id="3"
-            class="input-bordered input-primary input w-full max-w-xs bg-base-300"
-            type="text"
-            placeholder="ex: Panel"
-        />
+    <label class="label" for="3">Webpanel Title:</label>
+    <input
+      id="3"
+      class="input-bordered input-primary input w-full max-w-xs bg-base-300"
+      type="text"
+      placeholder="ex: Panel"
+    />
 
-        <div><input type="submit" value="Save" class="btn" /></div>
-    </form>
+    <label class="label text-2xl" for="3">Trusted Domains:</label>
+    <p>
+      To protect your machine from users uploading malware posing as
+      plugins/mods, you can limit URLs to download from such as <a
+        class="link link-primary"
+        href="https://modrinth.com">modrinth.com</a
+      >. Please seperate domains with commas.
+    </p>
+    <input
+      id="3"
+      class="input-bordered input-primary input w-full max-w-xs bg-base-300"
+      type="text"
+      placeholder="ex: Panel"
+    />
+
+    <div><input type="submit" value="Save" class="btn" /></div>
+  </form>
 </div>
