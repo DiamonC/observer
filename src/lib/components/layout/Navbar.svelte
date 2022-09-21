82a547c1fa940dc0776453cb325774121a0ddfc0
Jade Nash <jade.nashh18@gmail.com>
2022-09-21 16:32:26 -0500

    restored profile dropdown changes

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -81,6 +81,11 @@
                         tabindex="0"
                         class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-200 rounded-box w-52"
                     >
+                        <li>
+                            <p class="justify-between">
+                                DiamonC <span class="badge">Admin</span>
+                            </p>
+                        </li>
                         <li><a>Profile</a></li>
                         <li><a>Account Settings</a></li>
                         <li><a>Logout</a></li>
