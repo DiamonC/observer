82f1519e59180825020527fefe3cc2ff61f947f8
DiamonC <diamoncyt@gmail.com>
2022-10-10 18:52:05 -0500

    changed discord oauth to new backend

--- src/routes/(nologin)/signin/+page.svelte
+++ src/routes/(nologin)/signin/+page.svelte
@@ -1,31 +1,30 @@
 <script></script>
 
 <div class="hero min-h-screen bg-base-200">
-    <div class="hero-content text-center">
-        <div class="max-w-md">
-            <h1 class="text-5xl font-bold">Welcome!</h1>
-            <p class="py-6">
-                Sign in to access servers.
-            </p>
-            <div
-                class="mt-1 flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2 justify-center"
-            >
-                <a href="/signin/discord"><button class="btn btn-primary btn-icon-text text-2xs"
-                    ><img
-                        alt="microsoft logo"
-                        style="width:2.5ch"
-                        src="discord.ico"
-                    />Sign in with Discord</button
-                ></a>
-                <button class="btn btn-primary btn-icon-text text-2xs"
-                    ><img
-                        alt="metamastk logo"
-                        style="width:2.5ch"
-                        src="https://metamask.io/icons/icon-96x96.png?v=48400a28770e10dd52a8c0e539aeb282"
-                    />Sign in with MetaMask</button
-                >
-            </div>
-        </div>
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Welcome!</h1>
+      <p class="py-6">Sign in to access servers.</p>
+      <div
+        class="mt-1 flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2 justify-center"
+      >
+        <a href="https://api.arthmc.xyz/discord/login"
+          ><button class="btn btn-primary btn-icon-text text-2xs"
+            ><img
+              alt="microsoft logo"
+              style="width:2.5ch"
+              src="discord.ico"
+            />Sign in with Discord</button
+          ></a
+        >
+        <button class="btn btn-primary btn-icon-text text-2xs"
+          ><img
+            alt="metamastk logo"
+            style="width:2.5ch"
+            src="https://metamask.io/icons/icon-96x96.png?v=48400a28770e10dd52a8c0e539aeb282"
+          />Sign in with MetaMask</button
+        >
+      </div>
     </div>
+  </div>
 </div>
-
