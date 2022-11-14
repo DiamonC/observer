106695ae56371a9c80318f069644b5e381bced21
Leo 7 Christensen <114943151+sveltarne@users.noreply.github.com>
2022-11-14 16:48:46 +0000

    tweaked wordings, internationalized ServerCard

--- src/lib/components/ui/ServerCard.svelte
+++ src/lib/components/ui/ServerCard.svelte
@@ -2,6 +2,7 @@
   import { src_url_equal } from "svelte/internal";
   import { changeServerState } from "$lib/scripts/req.js";
   import { getServerInfo } from "$lib/scripts/req.js";
+	import { t, locale, locales } from "$lib/scripts/i18n"
   //Status variables
   let stopcolor = "info";
   let startcolor = "info";
@@ -110,11 +111,11 @@
       <div class="card-actions justify-end pt-4">
         <!-- placeholder for now? -->
         <div class="grow space-x-0.5">
-          <button class="btn btn-primary btn-sm h-9">Terminal</button>
+          <button class="btn btn-primary btn-sm h-9">{$t("button.terminal")}</button>
           <button type="submit" class="btn btn-{startcolor} btn-sm h-9"
             >{starttext}</button
           >
-          <button class="btn btn-{stopcolor} btn-sm h-9 stop-btn">Stop</button>
+          <button class="btn btn-{stopcolor} btn-sm h-9 stop-btn">{$t("button.stop")}</button>
         </div>
         <div class="self-center">
           <div class="badge badge-outline">1/20</div>
