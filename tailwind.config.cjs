f695d1af88eca499ab4919a447107936185d3b29
DiamonC <diamoncyt@gmail.com>
2022-11-26 22:31:22 -0600

    set theme back

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,17 +15,15 @@ module.exports = {
                     ],
                     primary: "#088385",
                     secondary: "#cc4304",
-                     "accent": "#1f242d",
-                     "base-100": "#2A303C",
+
                 },
                 light: {
                     ...require("daisyui/src/colors/themes")[
                         "[data-theme=light]"
                     ],
-                    "primary": "#088385",
-                    "secondary": "#cc4304",
-                    "base-100": "#798091",
-                    "accent": "#e5e6e6",
+                    primary: "#088385",
+                    secondary: "#cc4304",
+
                 },
             },
         ],
