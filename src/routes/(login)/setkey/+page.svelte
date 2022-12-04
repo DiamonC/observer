447e3e2eca51b03dad8d3f70ec19de15be5dc4b3
DiamonC <diamoncyt@gmail.com>
2022-12-03 22:01:21 -0600

    new page to grab key

---
+++ src/routes/(login)/setkey/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  //turn file into string, then into local storage
+  function storeKey() {
+    //take the file from the input and turn it into a string
+    let file = document.getElementById("key").files[0];
+    let reader = new FileReader();
+    reader.readAsText(file, "UTF-8");
+    reader.onload = function (evt) {
+      //store the string in local storage
+      localStorage.setItem("key", evt.target.result);
+      document.getElementById("use").classList.add("btn-disabled");
+    };
+  }
+</script>
+
+<div class="flex justify-center">
+  <input
+    type="file"
+    id="key"
+    class="file-input file-input-bordered file-input-secondary w-full max-w-xs"
+  />
+  <button class="btn btn-primary" id="use" on:click={storeKey}>Use</button>
+</div>
